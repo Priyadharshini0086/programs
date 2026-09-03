740d7b092f7b1adee6aee5dcd347ac1e3b7e164c
Priyadharshini0086 <kpdharshiniprasad00@gmail.com>
2026-09-03 19:03:01 +0530

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter a character";
+	cin>>ch;
+	switch(ch)
+	{
+		case 'a':
+			cout<<"It is a vowel";
+			break;
+		case 'e':
+		    cout<<"It is a vowel";
+			break;
+		case 'i':
+			cout<<"It is a vowel";
+			break;
+		case 'o':
+			cout<<"It is a vowel";	
+			break;
+		case 'u':
+			cout<<"It is a vowel";
+			break;
+		default:
+			cout<<"It is consonant";
+			
+	}
+	return 0;
+}
