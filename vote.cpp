2879a5c8bb41fca93d6824afb53b95f15651bed1
Priyadharshini0086 <kpdharshiniprasad00@gmail.com>
2026-09-03 19:02:39 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter a number";
+	cin>>age;
+	if(age>=18)
+	cout<<"Eligible to vote";
+	else
+	cout<<"Not Eligible to vote";
+	return 0;
+}
