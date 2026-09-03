5a69db469ff89ccdf4aa84d45a0fc6566cedfeb7
Priyadharshini0086 <kpdharshiniprasad00@gmail.com>
2026-09-03 19:02:10 +0530

    Add files via upload

---
+++ post,neg,0.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter a number";
+	cin>>n;
+	if(n>0)
+	cout<<"It is positive";
+	else if(n<0)
+	cout<<"It is negative";
+	else
+	cout<<"It is zero";
+	return 0;
+}
